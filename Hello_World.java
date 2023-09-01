df0caff4d90bc135286804d913176c0359dc65fd
Ryan Tusi <ryantusi45@gmail.com>
2023-09-01 11:41:00 +0530

    Create Hello_World.java
    As always, The ritual should prevail, print "Hello World!".
    "Hello World" - The 'Assalamualaikum' or 'Aloha' of programming languages

---
+++ Hello_World.java
@@ -0,0 +1,8 @@
+//As always, The ritual should prevail, print "Hello World!" 
+//"Hello World" - The 'Assalamualaikum' or 'Aloha' of programming languages
+
+public class Prog01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
