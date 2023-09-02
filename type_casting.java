cd3f6ffdbf7e5d641e3366819993f5e151e96686
Ryan Tusi <ryantusi45@gmail.com>
2023-09-02 11:42:50 +0530

    Create type_casting.java
    Implement Implicit and Explicit type casting

---
+++ type_casting.java
@@ -0,0 +1,14 @@
+public class Prog03 {
+	public static void main(String[] args) {
+		int a, b = 2, c;	//variable initialization 
+		a = 4;				// assigned initialization
+		
+		double d = 5.5;		
+		c = (int)d;			//explicit casting
+		
+		float f = 6.5f;		
+		d = f;				//implicit casting
+		
+		double e = d/(double)a; // both initialization and casting
+	}
+}
