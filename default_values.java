84c205cf7164b74a3feef0a7e3550c3928e43095
Ryan Tusi <ryantusi45@gmail.com>
2023-09-02 11:35:51 +0530

    Create default_values.java
    Curious to know default values of different data types? run the code

---
+++ default_values.java
@@ -0,0 +1,24 @@
+//declaring global variables of different data types to see their default values
+
+public class Prog02 {
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static boolean bo;
+	static float f;
+	static double d;
+	static char c;
+	
+	public static void main(String[] args) {
+		System.out.println("The default values are: ");
+		System.out.println("Byte: " + b);
+		System.out.println("Short: " + s);
+		System.out.println("Int: " + i);
+		System.out.println("Long: " + l);
+		System.out.println("Boolean: " + bo);
+		System.out.println("float: " + f);
+		System.out.println("Double: " + d);
+		System.out.println("Char: " + c);
+	}
+}
