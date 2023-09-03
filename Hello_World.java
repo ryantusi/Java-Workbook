bf9001a2ec89f038e643fb426f294add385472e4
Ryan Tusi <ryantusi45@gmail.com>
2023-09-03 12:14:17 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,7 +1,7 @@
 //As always, The ritual should prevail, print "Hello World!" 
 //"Hello World" - The 'Assalamualaikum' or 'Aloha' of programming languages
 
-public class Prog01 {
+public class Hello_World {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
