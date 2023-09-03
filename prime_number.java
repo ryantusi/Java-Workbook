8ed30856edfe31af12c7f1cbc29327803817eb2b
Ryan Tusi <ryantusi45@gmail.com>
2023-09-03 12:11:52 +0530

    Update prime_number.java

--- prime_number.java
+++ prime_number.java
@@ -1,21 +1,24 @@
+//import the scanner package to input values
 import java.util.Scanner;
 
 public class Prog04 {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);		//create an object to use the scanner
 		
-		System.out.println("Enter a number: ");
-		int n = scan.nextInt();
-		scan.close();
+		System.out.println("Enter a number: ");		//prompt the user
+		int n = scan.nextInt();				//use the scanner to take input from user
+		scan.close();					//close the scanner
 		boolean f = false; 	
-		
+
+		//calculation
 		for(int i=2; i<=n/2; i++) {
 			if(n%i==0) {
 				f = true;
 				break;
 			}
 		}
-		
+
+		//printing out based on results
 		if(f==false){
 			System.out.println("The number " + n + " is a prime number");
 		}else {
