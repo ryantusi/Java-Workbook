83c59efa57e2fa6ecce0582a7b2768b1dd10de28
Ryan Tusi <ryantusi45@gmail.com>
2023-09-04 11:34:14 +0530

    Create array_manipulation.java
    Ever wondered how arrays can be manipulated? Take a look at this program in which you will notice how to work with arrays.

---
+++ array_manipulation.java
@@ -0,0 +1,31 @@
+//have to import a package to manipulate arrays since arrays in default cannot be resized
+import java.util.Arrays;
+
+public class array_manipulation {
+	public static void main(String[] args) {
+    
+		//first original array
+		int orgarr[] = {1, 2, 3, 4, 5};
+		System.out.println("The original array: ");
+    
+		//printing the array with for loop
+		for(int i=0; i<orgarr.length; i++) {
+			System.out.print(orgarr[i] + " ");
+		}
+		System.out.println();
+
+		
+		//second array to resize
+		System.out.println("Resizing the array...");
+		int rearr[] = Arrays.copyOf(orgarr, 10);      //function to copy and resize the array
+		rearr[5] = 6;
+		rearr[6] = 7;
+		rearr[8] = 9;
+		rearr[7] = 8;
+		rearr[9] = 10;
+		System.out.println("after resize: ");
+    
+		//printing array with function
+		System.out.println(Arrays.toString(rearr));
+	}
+}
