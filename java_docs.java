38b3251aa489f99bf0b5aef82fd7c757681b9d08
Ryan Tusi <ryantusi45@gmail.com>
2023-09-06 12:10:19 +0530

    Create java_docs.java
    Implementations of Java Docs

---
+++ java_docs.java
@@ -0,0 +1,35 @@
+//this program should be viewed in browser to see the author and information of this program 
+
+/**
+ * 
+ * @author ryantusi
+ *
+ *
+ */
+
+public class Prog14 { 
+	/**
+	 * This program is for java exercise book to check javadoc and view in web browser
+	 * @param args
+	 */
+	 public static void main(String[] args) { // public, static, and void all is necessary for main function
+		/**
+		 * This is the main function and this program will add two numbers
+		 */
+		
+		int n1 = 2, n2 = 3;
+		/**
+		 * declaring variable
+		 */
+		
+		int sum = n1 + n2;
+		/**
+		 * computing sum
+		 */
+		System.out.println("The sum of the numbers " + n1 + " and " + n2 + " is: " + sum);
+		/**
+		 * printing result
+		 * @return null
+		 */
+	}
+}
