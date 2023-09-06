5d14beb17b6619b113b04aba088c3f9712e86571
Ryan Tusi <ryantusi45@gmail.com>
2023-09-06 11:58:27 +0530

    Create CL_arguments.java
    Ever wondered what command line arguments are and how to use it in your java programs. This module is just for this.

---
+++ CL_arguments.java
@@ -0,0 +1,15 @@
+//implementing the use of command line arguments
+
+public class CL_arguments {
+	public static void main(String args[]) {
+		/*the main method has a default array of string as an argument.
+      we can use it to to take command line arguments.
+      When you run the program manually, add your arguments with it.
+    */
+		
+		System.out.println("Your first line in argument is: " + args[0]);          //args[0] is the first argument of command line
+		//the above statement will work in JDK using the system's terminal/console
+    
+		
+	}
+}
