750d883b997bd957cdf5b1308aeb1612b17ee5f9
Ryan Tusi <ryantusi45@gmail.com>
2023-09-07 11:28:22 +0530

    Create number_in_words.java
    Ever wondered how to write numerical value in English words in java. This program uses switch case and reversing the number algorithm to produce each digit's word.

---
+++ number_in_words.java
@@ -0,0 +1,65 @@
+//Converting a number to words
+
+//import scan package
+import java.util.Scanner;
+
+public class Prog18 {
+	public static void main(String[] args) {
+    //object to scan
+		Scanner scan = new Scanner(System.in);
+
+    //prompting user for number
+		System.out.println("Enter number: ");
+		int num = scan.nextInt();
+		int temp = 0;
+
+    //reversing the number and storing it in temp
+		while(num>0) {
+			temp += num%10;
+			temp *= 10;
+			num /= 10;
+		}
+
+    //printing out the number
+		System.out.println("The number in word is: ");
+
+    //reversing the string back to original with each number as the word
+		while(temp>0) {
+			num = temp%10;
+			switch(num) {
+			case 0:
+				break;
+			case 1:
+				System.out.print(" one ");
+				break;
+			case 2:
+				System.out.print(" two ");
+				break;
+			case 3:
+				System.out.print(" three ");
+				break;
+			case 4:
+				System.out.print(" four ");
+				break;
+			case 5:
+				System.out.print(" five ");
+				break;
+			case 6:
+				System.out.print(" six ");
+				break;
+			case 7: 
+				System.out.print(" seven ");
+				break;
+			case 8:
+				System.out.print(" eight ");
+				break;
+			case 9:
+				System.out.print(" nine ");
+				break;
+			default:
+				System.out.println("invalid");
+			}
+			temp /= 10;
+		}
+	}
+}
