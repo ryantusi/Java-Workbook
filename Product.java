deb1b1f048b3928ea405e2dc2001790ace65bdb3
Ryan Tusi <ryantusi45@gmail.com>
2023-09-13 11:59:49 +0530

    Create Product.java
    Basic program to illustrate multiplication in java

---
+++ Product.java
@@ -0,0 +1,17 @@
+//basic program to illustrate multiplication
+
+public class FiveIntegerProduct {
+	public static void main(String[] args) {
+		int number1 = 11;
+		int number2 = 22;
+		int number3 = 33;
+		int number4 = 44;
+		int number5 = 55;
+		int product;
+		
+		product = number1*number2*number3*number4*number5;
+		
+		System.out.println("The product is: ");
+		System.out.print(product);
+	}
+}
