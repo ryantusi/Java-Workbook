bd81160b815028ba4de49afc644b90532b517e8a
Ryan Tusi <ryantusi45@gmail.com>
2023-09-13 12:00:46 +0530

    Create Sum.java
    Basic program to illustrate addition in java

---
+++ Sum.java
@@ -0,0 +1,19 @@
+//basic program to illustrate addition in java
+
+public class Sum {
+	public static void main(String[] args) {
+		int number1 = 11;
+		int number2 = 22;
+		int number3 = 33;
+		int number4 = 44;
+		int number5 = 55;
+		int number6 = 66;
+		int number7 = 77;
+		int sum;
+		
+		sum = number1 + number2 + number3 + number4 + number5 + number6 + number7;
+		
+		System.out.println("The sum is: ");
+		System.out.print(sum);
+	}
+}
