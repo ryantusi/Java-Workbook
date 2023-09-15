d0bd6d461335b86d0da17ca192fc078cd0179f4d
Ryan Tusi <ryantusi45@gmail.com>
2023-09-15 08:20:21 +0530

    Create multiplyEquations.java

---
+++ multiplyEquations.java
@@ -0,0 +1,16 @@
+public class multiplyEquations {
+	public static void main(String[] args) {
+		int n1 = 98;
+		int n2 = 5;
+		int x, y;
+		
+		x = 31*n1;
+		y = 17*n2;
+		
+		System.out.println("The value of two equations is: ");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println("The sum of the two values of equation is: ");
+		System.out.print(x+y);
+	}
+}
