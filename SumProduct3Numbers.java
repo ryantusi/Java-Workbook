7f24a2a9d75486aeee9941ab912cbaa7503ad2a3
Ryan Tusi <ryantusi45@gmail.com>
2023-09-15 08:17:31 +0530

    Create SumProduct3Numbers.java
    Basic Arithmatic exercise using addition and multiplication of 3 variables

---
+++ SumProduct3Numbers.java
@@ -0,0 +1,15 @@
+//demonstrating basic arithmatic problems using addition and multiplication
+
+public class SumProduct3Numbers {
+	public static void main(String[] args) {
+		int number1 = 98;
+		int number2 = 5;
+		int number3 = 77;
+		
+		int sum, product;
+		sum = number1+number2+number3;
+		product = number1*number2*number3;
+		
+		System.out.println("The sum and product of the numbers " + number1 + ", " + number2 + ", and " + number3 + " is " + sum + " and " + product);
+	}
+}
