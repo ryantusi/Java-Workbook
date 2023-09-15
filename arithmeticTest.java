2aa8cb311f37d87e957847fdab291ecbeeb5c0a4
Ryan Tusi <ryantusi45@gmail.com>
2023-09-15 08:19:18 +0530

    Create arithmeticTest.java
    Illustration of all arithmetic concepts of sum, multiplication, division, difference, and remainder.

---
+++ arithmeticTest.java
@@ -0,0 +1,17 @@
+//Illustration of all arithmetic concepts
+
+public class arithmeticTest {
+	public static void main(String[] args) {
+		int n1 = 98;
+		int n2 = 5;
+		int sum, difference, product, qoutient, remainder;
+		
+		sum = n1 + n2;
+		difference = n1 -n2;
+		product = n1*n2;
+		qoutient = n1/n2;
+		remainder = n1%n2;
+		
+		System.out.println("The sum, difference, product, qoutient, remainder of " + n1 + " and " + n2 + " are " + sum + ", " + difference + ", " + product + ", " + qoutient + ", and " + remainder );
+	}
+}
