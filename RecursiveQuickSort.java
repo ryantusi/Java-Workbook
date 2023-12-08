ac88b6a96426c244e682395a4e17ed41f7c74016
Ryan Tusi <ryantusi45@gmail.com>
2023-12-08 11:39:01 +0530

    Create RecursiveQuickSort.java

---
+++ RecursiveQuickSort.java
@@ -0,0 +1,55 @@
+// Implementation of Quick Sort Algorithm in Java
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class RecursiveQuickSort {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter size of an array: ");
+		int size = scan.nextInt();
+		System.out.println("Enter elements: ");
+		int[] array  = new int[size];
+		for(int i=0; i<size; i++) {
+			array[i] = scan.nextInt();
+		}
+		
+		int fidx = 0;
+		int lidx = array.length - 1;
+		
+		System.out.println("Your array: " + Arrays.toString(array));
+		System.out.println("Sorting...");
+		quickSort(array, fidx, lidx);
+		System.out.println("Sorted: " + Arrays.toString(array));
+	}
+	public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+  
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+  
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            
+            //recursion
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+}
